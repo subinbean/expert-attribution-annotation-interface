c9fa8c0966c36ac30ae3a0751814a4993d421ffe
Subin Lee <subin1719@gmail.com>
2023-05-10 23:51:30 -0400

    minor fix

--- backend/routes.js
+++ backend/routes.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res) => {
-    res.json({msg: 'this works!'})
+    res.json({msg: 'this works!!'})
 })
 
 module.exports = router
