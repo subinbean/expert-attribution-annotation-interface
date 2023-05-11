541dd30dd07fec0d1fb3e1ed1197363a6b58ec5b
Subin Lee <subinlee@subins-mbp-2.mynetworksettings.com>
2023-05-10 23:48:22 -0400

    added router

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,19 @@
+require('dotenv').config()
+
 const express = require('express')
 
 const app = express()
- 
-// routes
-app.get('/', (req, res) => {
-    res.json({msg: 'Welcome to the app'})
-})
 
-app.listen(4000, () => {
-    console.log('Listening on port 4000')
+const routes = require('./routes.js')
+ 
+// middleware for logging requests
+app.use((req, res, next) => {
+    console.log(req.path, req.method)
+    next()
+})
+// routes
+app.use(routes)
+
+app.listen(process.env.PORT, () => {
+    console.log('Listening on port', process.env.PORT)
 })
