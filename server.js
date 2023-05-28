1320c6c7bcf51778a6465f30ea4ad3add5f62562
Subin Lee <subin1719@gmail.com>
2023-05-28 20:24:21 +0900

    Mongo integrated

--- server.js
+++ server.js
@@ -1,18 +1,32 @@
-require('dotenv').config()
-
 const express = require('express')
+const mongoose = require('mongoose')
+require('dotenv').config()
 
 const app = express()
 
-const routes = require('./routes.js')
- 
+// MongoDB Connection
+mongoose.set('strictQuery',false)
+mongoose.connect(process.env.MONGODB_URI)
+
+const noteSchema = new mongoose.Schema({
+    content: String,
+    important: Boolean,
+  })
+  
+const Note = mongoose.model('Note', noteSchema)
+
 // middleware for logging requests
 app.use((req, res, next) => {
     console.log(req.path, req.method)
     next()
 })
+
 // routes
-app.use(routes)
+app.get('/api/questions', (request, response) => {
+    Note.find({}).then(notes => {
+        response.json(notes)
+    })
+})
 
 app.listen(process.env.PORT, () => {
     console.log('Listening on port', process.env.PORT)
