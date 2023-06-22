daaf2bb7c70f912237175fee9cac856c78f7da28
Subin Lee <subin1719@gmail.com>
2023-06-22 16:49:20 -0400

    database change

--- schema.js
+++ schema.js
@@ -29,5 +29,5 @@ const question = new mongoose.Schema({
     time_spent: Number
 })
 
-module.exports = mongoose.model('Question', question)
+module.exports = mongoose.model('Question', question, 'expert-pilot')
 
