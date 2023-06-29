a0dd5a83028bf8625c57517229cf6521b92ed059
Subin Lee <subin1719@gmail.com>
2023-06-29 17:45:50 -0400

    part 1 db

--- schema.js
+++ schema.js
@@ -31,5 +31,5 @@ const question = new mongoose.Schema({
     time_spent: Number
 })
 
-module.exports = mongoose.model('Question', question, 'expertqa-stage2-pilot')
+module.exports = mongoose.model('Question', question, 'expertqa-stage2-part1')
 
