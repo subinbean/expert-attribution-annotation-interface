a070ba536aa7cf508479dc8db70192565a7ef613
Subin Lee <subin1719@gmail.com>
2023-10-16 21:15:10 -0400

    updated README for other people to use

--- README.md
+++ README.md
@@ -1,7 +1,19 @@
 ### Description
 
-This repo contains the code for the annotation interface used for "ExpertQA: Expert-Curated Questions and Attributed Answers." Use npm run inside the frontend folder to run the frontend and npm run dev to run the backend.
+This repo contains the code for the annotation interface used for "ExpertQA: Expert-Curated Questions and Attributed Answers." You can find the paper here: https://arxiv.org/abs/2309.07852
+
+### Installation
+
+Run `npm install` in both the root directory (backend) as well as the frontend directory to install all necessary packages. Make sure to run it in both folders to download libraries for both.
+
+### Running the app
+
+First, create a `.env` file in the root directory with a `PORT=` variable to specify what port you want the server to run as well as `MONGODB_URI=` variable to connect to a MongoDB database. Then, use `npm run dev` in the root folder to start the backend server. Finally, use `npm run` inside the frontend folder to start the frontend.
 
 ### Stack
 
 This application uses the MERN stack--React in the frontend, Express / Node.js as the backend, and MongoDB as the database
+
+### Structure
+
+The backend server code is contained in `server.js` with the schema specified in `schema.js`. All the frontend code resides in the `frontend` folder with React components in `components` and different pages in `pages` routed by the react router in `App.js`.
--- schema.js
+++ schema.js
@@ -1,34 +1,35 @@
 const mongoose = require("mongoose");
 
 const claim = new mongoose.Schema({
-  claim_string: String,
-  evidence: [String],
-  support: String,
-  reason_missing_support: String,
-  reliability: String,
-  informativeness: String,
-  worthiness: String,
-  correctness: String,
-  revised_claim: String,
-  revised_evidence: String,
+    claim_string: String,
+    evidence: [String],
+    support: String,
+    reason_missing_support: String,
+    reliability: String,
+    informativeness: String,
+    worthiness: String,
+    correctness: String,
+    revised_claim: String,
+    revised_evidence: String,
 });
 
 const question = new mongoose.Schema({
-  completed: Boolean,
-  question_string: String,
-  answer_string: String,
-  attribution: [String],
-  answer_origin_model: String,
-  annotator_id: String,
-  metadata: {
-    question_type: String,
-    field: String,
-    specific_field: String,
-  },
-  claims: [claim],
-  usefulness: String,
-  revised_answer: String,
-  time_spent: Number,
+    completed: Boolean,
+    question_string: String,
+    answer_string: String,
+    attribution: [String],
+    answer_origin_model: String,
+    annotator_id: String,
+    metadata: {
+        question_type: String,
+        field: String,
+        specific_field: String,
+    },
+    claims: [claim],
+    usefulness: String,
+    revised_answer: String,
+    time_spent: Number,
 });
 
+// modify the 3rd parameter to specify which MongoDB collection to use
 module.exports = mongoose.model("Question", question, "expertqa-stage2-0710");
